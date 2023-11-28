1f0f43618c1ac7225f6af711ff0f6d085003e6c9
Alec Croue <acroue@paul-f3Ar2s1.clusters.42paris.fr>
2023-11-28 17:54:10 +0100

    push main func with norm and makefile

---
+++ get_next_line.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acroue <acroue@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/28 17:44:01 by acroue            #+#    #+#             */
+/*   Updated: 2023/11/28 17:44:01 by acroue           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+#include <stdio.h>
+
+int	is_newline(char *new)
+{
+	int	i;
+
+	i = -1;
+	if (!new)
+		return (0);
+	while (new[++i])
+		if (new[i] == '\n')
+			return (i + 1);
+	return (0);
+}
+
+int	read_buffer(int fd, char *buffer)
+{
+	if (buffer[0] != '\0')
+	{
+		return (1);
+	}
+	if (read(fd, buffer, BUFFER_SIZE) > 0)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = -1;
+	if (!dest && !src)
+		return (NULL);
+	while (++i < BUFFER_SIZE)
+	{
+		if (n && i < BUFFER_SIZE - n)
+		{
+			((char *)dest)[i] = ((char *)src)[i];
+		}
+		else
+			((char *)dest)[i] = '\0';
+	}
+	return (dest);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE];
+	char		*tmp;
+	int			nl;
+
+	tmp = NULL;
+	while ((!is_newline(tmp)) && read_buffer(fd, (char *)buffer) > 0)
+	{
+		nl = is_newline(buffer);
+		tmp = ft_strnjoin(tmp, buffer, nl);
+		if (!tmp)
+			return (NULL);
+		ft_memmove(buffer, &buffer[nl], nl);
+	}
+	return (tmp);
+}
---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acroue <acroue@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 11:58:45 by acroue            #+#    #+#             */
+/*   Updated: 2023/11/28 17:50:05 by acroue           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+int		ft_strlen(const char *str);
+char	*ft_strnjoin(char *s1, char *s2, int n);
+int		is_newline(char *new);
+int		read_buffer(int fd, char *buffer);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+char	*get_next_line(int fd);
+
+#endif
